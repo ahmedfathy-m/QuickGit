083d51365c9ae0b75650a403287d0757a79cd79f
ahmedfathy-m <ahmedfathy.mha@gmail.com>
2022-08-10 19:38:18 +0200

    Initial Commit

---
+++ QuickGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickGit
+//
+//  Created by Ahmed Fathy on 10/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
